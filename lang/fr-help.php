fc2f4b2f92c527da8e7cea5671d71fd20f8ae5f9
nIQnutn <sn@niqnutn.com>
2016-06-11 19:44:10 +0200

    Update fr-help.php

--- lang/fr-help.php
+++ lang/fr-help.php
@@ -1,14 +1,74 @@
 <?php if (!defined('PLX_ROOT')) exit; ?>
-<p>PluW3css</p>
+<h1>W3css: Th&egrave;me pour PluXml</h1>
 
-<p>Pr&eacute;-requis</p>
+<p>
+Le th&egrave;me W3css reprend le framework du m&ecirc;me nom.
+La structure et l'organisation des pages est fortement inspir&eacute; du th&egrave;me d'origine de PluXml.
+L'objectif est de fournir un th&egrave;me &eacute;lgant tout en profitant des autres &eacute;l&eacute;ments de pr&eacute;sentation de W3css  pour aboutir &agrave; un site/blog homog&egrave;ne.
+</p>
+
+<p>Le th&egrave;me W3css est disponible sur <a href="https://blog.niqnutn.com/plugins/repository/index.php">http://blog.niqnutn.com/plugins/repository/index.php</a> ou <a href="https://github.com/nIQnutn/PluXml-W3css">https://github.com/nIQnutn/PluXml-W3css</a>.</p>
+
+<p>Le th&egrave;me est disponible sous la licence XXXX.</p>
+
+
+<h2>Fonctions principales</h2>
+
 <ul>
-<li>Plugin Font-Awesome</li>
-<li>Plugin MyPager (modifi&eacute;)</li>
-
-
-Documentation
-
-
+	<li>Un menu d&eacute;roulant</li>
+	<li>Th&egrave;me responsive pour s'adpater &agrave; tous les &eacute;crans</li>
+	<li>Int&eacute;gration de la police d'ic&ocirc;nes FontAwesome ( <a href="https://blog.niqnutn.com/plugins/repository/index.php">http://blog.niqnutn.com/plugins/repository/index.php</a> ou <a href="https://github.com/nIQnutn/plxFontAwesome">https://github.com/nIQnutn/plxFontAwesome</a>)</li>
+	<li>Un th&egrave;me de couleur modifiable (avec le plugin  plxW3cssThemeSelect <a href="https://blog.niqnutn.com/plugins/repository/index.php">http://blog.niqnutn.com/plugins/repository/index.php</a> ou <a href="https://github.com/nIQnutn?tab=repositories">https://github.com/nIQnutn?tab=repositories</a>)</li>
+	<li>Un th&egrave;me de couleur que l'on peut cr&eacute;er et ajouter &agrave; facilement <a href="http://www.w3schools.com/w3css/w3css_color_themes.asp">http://www.w3schools.com/w3css/w3css_color_themes.asp</a></li>
+	<li>Une pagination des articles simplifi&eacute;e</li>
 </ul>
 
+<h2>Pr&eacute;-requis</h2>
+<ul>
+	<li>PluXml 5.5</li>
+	<li>Plugin plxFontAwesome  <a href="https://blog.niqnutn.com/plugins/repository/index.php">http://blog.niqnutn.com/plugins/repository/index.php</a> ou <a href="https://github.com/nIQnutn/plxFontAwesome">https://github.com/nIQnutn/plxFontAwesome</a></li>
+	<li>Plugin plxSimplePager  <a href="https://blog.niqnutn.com/plugins/repository/index.php">http://blog.niqnutn.com/plugins/repository/index.php</a> ou <a href="https://github.com/nIQnutn/plxSimplePager">https://github.com/nIQnutn/plxSimplePager</a></li>
+</ul>
+
+<h2>Documentation</h2>
+
+<h3>Installation du th&egrave;me</h3>
+
+<p>Pour installer le th&egrave;me, t&eacute;l&eacute;charger le directement depuis  <a href="https://blog.niqnutn.com/plugins/repository/index.php">http://blog.niqnutn.com/plugins/repository/index.php</a> ou <a href="https://github.com/nIQnutn/PluXml-W3css">https://github.com/nIQnutn/PluXml-W3css</a>.</p>
+<p>Une fois l'archive décompressée, il faut envoyer le dossier "W3css" via FTP sur votre site et le déposer dans "/themes".</p>
+<p>Après avoir copié le th&egrave;me dans votre PluXml, il ne vous reste plus qu’à l’activer. Pour ça, il faut se connecter sur le site en tant qu'administrateur et aller dans le menu: Paramètres > Th&egrave;mes.</p>
+
+<h3>Modifier la couleur du th&egrave;me</h3>
+
+<p>Il existe deux solutions pour modifier la couleur de votre th&egrave;me: modifier directement la feuille de style ou utiliser le plugin plxW3cssThemeSelect.</p>
+
+<p>La solution la plus simple est d'installer et activer le plugin plxW3cssThemeSelect.</p>
+<p>Une fois le th&egrave;me activ&eacute;, il faut aller dans la configuration du plugin et s&eacute;lection la couleur.</p>
+<p>Une fois valid&eacute;, il faut recharger la page de votre site pour que les modifications s'appliquent.</p>
+
+<p>Il est possible de cr&eacute;er diff&eacute;rents th&egrave;mes de couleur sur  <a href="http://www.w3schools.com/w3css/w3css_color_themes.asp">http://www.w3schools.com/w3css/w3css_color_themes.asp</a>. 
+Il suffit de r&eacute;cup&eacute;rer le CSS qui a &eacute;t&eacute; g&eacute;n&eacute;r&eacute; et le recopier dans un fichier que l'on peut nommer <strong>couleur.css</strong>. 
+il faut ensuite envoyer le fichier "couleur.css" via FTP sur votre site et le déposer dans "/plugins/plxW3cssThemeSelect/themes" .
+</p>
+
+
+<p>La deuxi&egrave;me solution consiste &agrave; d'&eacute;diter directement la feuille </p>
+<p>R&eacute;cup&eacute;rer et &eacute;diter la feuille de style /themes/w3css/css/w3-custom.css</p>
+<p>Une fois les modifications effectu&eacute;es, il faut recharger la page de votre site pour que les modifications s'appliquent</p>
+<p>Cette solution &eacute;vite de charger une feuille de style suppl&eacute;mentaire.</p>
+
+<h3>D&eacute;couvrir W3css</h3>
+
+<a href="../../themes/w3css/w3css.html">D&eacute;couvrir W3css</a>
+
+<div class="w3-container w3-red">
+  <h3><i class="fa fa-lg  fa-times-circle"></i> Danger!</h3>
+  <p>Red often indicates a dangerous or potentially negative action.</p>
+</div>
+
+<h2>Ressources</h2>
+
+<ul>
+	<li><a href="http://www.w3schools.com/w3css/default.asp">La documentation compl&egrave;te de W3css</a></li>
+	<li><a href="http://forum.pluxml.org/viewtopic.php?id=5576">La discussion autour du th&egrave;me sur le forum PluXml</a></li>
+</ul>
